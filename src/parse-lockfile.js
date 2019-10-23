c1cd93c6c8394ff6b26aa96638c6c649b76b3c9a
Em McDonald <mcdonald.emily@live.co.uk>
2019-10-23 16:35:59 +0100

    :sparkles: Extended tool to gather mutliple versions of requested packages

--- src/parse-lockfile.js
+++ src/parse-lockfile.js
@@ -1,11 +1,39 @@
 module.exports = contents =>
-  contents.match(/^\S.*\:\n\s\sversion.*$/gm).reduce((acc, pkg) => {
-    const parsed = /^\"?(?<name>.+)@(?<requested>\^?\d\.\d\.\d)\"?\:\n\s\s.+\"(?<resolved>.+)\"/gm.exec(
-      pkg
-    );
+  contents
+    .match(/^\S.*\:\n\s\sversion.*$/gm)
+    .reduce((accumulatedPackages, pkg) => {
+      const [requestedLine, versionLine] = pkg.split("\n");
 
-    if (parsed && parsed.groups)
-      acc.push({ ...parsed.groups, requested: [parsed.groups.requested] });
+      let name;
 
-    return acc;
-  }, []);
+      const requestedPackages = requestedLine.split(",");
+      const listOfRequestedPackages =
+        requestedPackages &&
+        requestedPackages.reduce(
+          (accumulatedRequestedPackages, requestedPackage) => {
+            const parsed = /^\"?(?<name>.+)@(?<requested>\^?\d\.\d\.\d)\"?/gm.exec(
+              requestedPackage
+            );
+            if (parsed && parsed.groups) {
+              if (!name) name = parsed.groups.name.trim();
+              accumulatedRequestedPackages.push(parsed.groups.requested);
+            }
+
+            return accumulatedRequestedPackages;
+          },
+          []
+        );
+
+      const parsedVersion = /^.+\"(?<version>.+)\"$/.exec(versionLine);
+      const resolvedVersion =
+        parsedVersion && parsedVersion.groups && parsedVersion.groups.version;
+
+      if (listOfRequestedPackages && resolvedVersion)
+        accumulatedPackages.push({
+          name,
+          requested: listOfRequestedPackages,
+          resolved: resolvedVersion
+        });
+
+      return accumulatedPackages;
+    }, []);
--- src/parse-lockfile.spec.js
+++ src/parse-lockfile.spec.js
@@ -19,10 +19,24 @@ describe("parseLockfile", () => {
     { name: "@baz/baz", requested: ["^3.2.3"], resolved: "3.2.4" }
   ];
 
+  const expectedMultiStandard = [
+    { name: "foo", requested: ["^1.2.3", "^1.2.4"], resolved: "1.2.5" },
+    { name: "bar", requested: ["^2.2.3", "^2.2.4"], resolved: "2.2.5" },
+    { name: "baz", requested: ["^3.2.3", "^3.2.4"], resolved: "3.2.5" }
+  ];
+
+  const expectedMultiCustom = [
+    { name: "@foo/foo", requested: ["^1.2.3", "^1.2.4"], resolved: "1.2.5" },
+    { name: "@bar/bar", requested: ["^2.2.3", "^2.2.4"], resolved: "2.2.5" },
+    { name: "@baz/baz", requested: ["^3.2.3", "^3.2.4"], resolved: "3.2.5" }
+  ];
+
   it.each`
-    file          | expected
-    ${"standard"} | ${expectedStandard}
-    ${"custom"}   | ${expectedCustom}
+    file                | expected
+    ${"standard"}       | ${expectedStandard}
+    ${"custom"}         | ${expectedCustom}
+    ${"multi-standard"} | ${expectedMultiStandard}
+    ${"multi-custom"}   | ${expectedMultiCustom}
   `("should parse $file char packages", ({ file, expected }) => {
     const lockfile = readLockfile(`${file}.lock`);
     const actual = parseLockfile(lockfile);
