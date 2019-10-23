2b48de3c5ade45e6c9dd1704856a3b8d6f8de6d3
Em McDonald <mcdonald.emily@live.co.uk>
2019-10-23 15:40:59 +0100

    :art: Reduce code duplication in tests

--- src/parse-lockfile.spec.js
+++ src/parse-lockfile.spec.js
@@ -7,28 +7,25 @@ const readLockfile = lockfile =>
   fs.readFileSync(path.join(__dirname, "/test-data/", lockfile)).toString();
 
 describe("parseLockfile", () => {
-  it("should parse standard char packages", () => {
-    const standardLock = readLockfile("standard.lock");
+  const expectedStandard = [
+    { name: "foo", requested: "^1.2.3", resolved: "1.2.4" },
+    { name: "bar", requested: "^2.2.3", resolved: "2.2.4" },
+    { name: "baz", requested: "^3.2.3", resolved: "3.2.4" }
+  ];
 
-    const expected = [
-      { name: "foo", requested: "^1.2.3", resolved: "1.2.4" },
-      { name: "bar", requested: "^2.2.3", resolved: "2.2.4" },
-      { name: "baz", requested: "^3.2.3", resolved: "3.2.4" }
-    ];
-    const actual = parseLockfile(standardLock);
+  const expectedCustom = [
+    { name: "@foo/foo", requested: "^1.2.3", resolved: "1.2.4" },
+    { name: "@bar/bar", requested: "^2.2.3", resolved: "2.2.4" },
+    { name: "@baz/baz", requested: "^3.2.3", resolved: "3.2.4" }
+  ];
 
-    expect(actual).toEqual(expected);
-  });
-
-  it("should parse custom char packages", () => {
-    const customLock = readLockfile("custom.lock");
-
-    const expected = [
-      { name: "@foo/foo", requested: "^1.2.3", resolved: "1.2.4" },
-      { name: "@bar/bar", requested: "^2.2.3", resolved: "2.2.4" },
-      { name: "@baz/baz", requested: "^3.2.3", resolved: "3.2.4" }
-    ];
-    const actual = parseLockfile(customLock);
+  it.each`
+    file          | expected
+    ${"standard"} | ${expectedStandard}
+    ${"custom"}   | ${expectedCustom}
+  `("should parse $file char packages", ({ file, expected }) => {
+    const lockfile = readLockfile(`${file}.lock`);
+    const actual = parseLockfile(lockfile);
 
     expect(actual).toEqual(expected);
   });
